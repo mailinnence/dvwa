d2b1aec56e5050c007a9bdc80ce4cdf2622c4879
mailinnence <78959189+mailinnence@users.noreply.github.com>
2021-10-30 22:12:12 +0900

    Add files via upload

---
+++ 0.db_info.php
@@ -0,0 +1,7 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "dvwa1");
+//꼭 하나의 파일에 mysqli_connect()을 만들고 include로 파일을 가져오면서 하도록해라 나중에 처리해야할 파일이 늘어나면 골치 아프다
+//dvwa 학습파일명에서 끝에서 숫자는 1=row 2=middle 3.high 4.impossible 을 의미한다
+
+?>
